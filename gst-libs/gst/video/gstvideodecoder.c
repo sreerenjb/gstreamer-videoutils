GStreamer/gstreamer

videodecoder: Only keep track of timestamps if the subclass is parsing data
Otherwise we just pass through the timestamps directly and don't
need to waste additional memory for them.

Fixes bug #689814.

gst-libs/gst/video/gstvideodecoder.c
@@ -1678,7 +1678,7 @@ gst_video_decoder_chain_forward (GstVideoDecoder * decoder,
   if (priv->current_frame == NULL)
     priv->current_frame = gst_video_decoder_new_frame (decoder);
 
-  if (GST_BUFFER_PTS_IS_VALID (buf)) {
+  if (GST_BUFFER_PTS_IS_VALID (buf) && !priv->packetized) {
     gst_video_decoder_add_timestamp (decoder, buf);
   }
   priv->input_offset += gst_buffer_get_size (buf);

sreerenjb/gstreamer-videoutils

Conflicts:
	gst-libs/gst/video/gstvideodecoder.c

gst-libs/gst/video/gstvideodecoder.c
@@ -1611,7 +1611,7 @@ gst_video_decoder_chain_forward (GstVideoDecoder * decoder,
   if (priv->current_frame == NULL)
     priv->current_frame = gst_video_decoder_new_frame (decoder);
 
-  if (GST_BUFFER_TIMESTAMP_IS_VALID (buf)) {
+  if (GST_BUFFER_TIMESTAMP_IS_VALID (buf) && !priv->packetized) {
     gst_video_decoder_add_timestamp (decoder, buf);
   }
   priv->input_offset += GST_BUFFER_SIZE (buf);